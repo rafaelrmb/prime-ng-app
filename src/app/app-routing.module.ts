e398da2f86cf608f21ae857e8a8f9254c5cfd296
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-05-29 17:01:10 -0300

    add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./patient-management/patient-management.component').then(m => m.PatientManagementComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
