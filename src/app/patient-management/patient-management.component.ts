68ef6d96981dbe2b434fbc90545ec8abcfed708b
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-05-29 17:01:41 -0300

    add patient management component

---
+++ src/app/patient-management/patient-management.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-patient-management',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './patient-management.component.html',
+  styleUrls: ['./patient-management.component.scss']
+})
+export class PatientManagementComponent {
+
+}
