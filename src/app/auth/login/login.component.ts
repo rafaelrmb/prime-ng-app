1ca0ca54f29a49420a114c99decb1b856bd3f2a0
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-06-05 17:34:12 -0300

    add login component done

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    ButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  signInForm!: FormGroup;
+  loading = false;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  onSubmit() {
+    this.load();
+    console.log(this.signInForm.value);
+    this.load();
+  }
+
+  load() {
+    this.loading = !this.loading
+  }
 }
